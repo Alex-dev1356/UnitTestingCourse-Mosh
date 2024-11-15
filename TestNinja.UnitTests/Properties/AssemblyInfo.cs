12482365bc68cf264eb43e8c6ca328bcec015a3a
Alexander Soliven <asoliven26@yahoo.com>
2024-11-15 17:08:51 +0800

    Creating First Unit Test Using MS Test, and refactored the Reservation.cs with confidence after the writing of test.

---
+++ TestNinja.UnitTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("TestNinja.UnitTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("CIO")]
+[assembly: AssemblyProduct("TestNinja.UnitTests")]
+[assembly: AssemblyCopyright("Copyright © CIO 2024")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("8042d2db-9826-495d-b9b0-6791fb370d80")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -0,0 +1,47 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using TestNinja.Fundamentals;
+using System;
+
+namespace TestNinja.UnitTests
+{
+    [TestClass]
+    public class ReservationTests
+    {
+        [TestMethod]
+        //Good Naming Convention in naming the method you have to test: NameOfTheMethod_Scenario_ExpectedBehaviour()
+        public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+        {
+            //3 parts of every test method: Arrange, Act and Assert
+
+            //Arrange - Where we initialized our objects
+            var reservation = new Reservation();
+
+            //Act - Here we specify the method or function that we want to test and supply the parameters/arguments needed
+            var result = reservation.CanBeCancelledBy( new User() {IsAdmin = true });
+
+            //Assert - Here we verify that result if correct.
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanBeCancelledBy_SameUsercCanceling_ReturnsTrue()
+        {
+            var user = new User();
+            var reservation = new Reservation();
+
+            var result = reservation.CanBeCancelledBy(reservation.MadeBy = user);
+
+            Assert.IsTrue(result);
+        }
+
+        [TestMethod]
+        public void CanbeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+        {
+            var reservation = new Reservation { MadeBy = new User() };
+
+            var result = reservation.CanBeCancelledBy(new User());
+
+            Assert.IsFalse(result);
+        }
+    }
+}
