05467661d38a32657430ad5222058dda8572888d
Alex-dev1356 <asoliven26@yahoo.com>
2024-11-17 11:56:16 +0800

    Adding NUnit Framework and Adding Info/Instruction Class for Installation

---
+++ TestNinja.UnitTests/InformationsAndInstructions.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestNinja.UnitTests
+{
+    public class InformationsAndInstructions
+    {
+        //Installing NUnit Test Framework: https://www.nuget.org/packages/NUnit
+        //Open Package Manager Console make sure the selected "Default Project" is TestNinja.UnitTests and run: Install-Package NUnit -Version 3.8.1
+
+        //Installing MSTest.TestAdapter: https://www.nuget.org/packages/MSTest.TestAdapter
+        //Open Package Manager Console make sure the selected "Default Project" is TestNinja.UnitTests and run: Install-Package NUnit3TestAdapter -Version 3.8.0
+    }
+}
--- TestNinja.UnitTests/ReservationTests.cs
+++ TestNinja.UnitTests/ReservationTests.cs
@@ -1,13 +1,60 @@
-﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+﻿//using Microsoft.VisualStudio.TestTools.UnitTesting;
 using TestNinja.Fundamentals;
 using System;
+using NUnit.Framework;
 
 namespace TestNinja.UnitTests
 {
-    [TestClass]
+    //<---- Using MS Test ----->
+    #region
+    //[TestClass]
+    //public class ReservationTests
+    //{
+    //    [TestMethod]
+    //    //Good Naming Convention in naming the method you have to test: NameOfTheMethod_Scenario_ExpectedBehaviour()
+    //    public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
+    //    {
+    //        //3 parts of every test method: Arrange, Act and Assert
+
+    //        //Arrange - Where we initialized our objects
+    //        var reservation = new Reservation();
+
+    //        //Act - Here we specify the method or function that we want to test and supply the parameters/arguments needed
+    //        var result = reservation.CanBeCancelledBy(new User() { IsAdmin = true });
+
+    //        //Assert - Here we verify that result if correct.
+    //        Assert.IsTrue(result);
+    //    }
+
+    //    [TestMethod]
+    //    public void CanBeCancelledBy_SameUsercCanceling_ReturnsTrue()
+    //    {
+    //        var user = new User();
+    //        var reservation = new Reservation();
+
+    //        var result = reservation.CanBeCancelledBy(reservation.MadeBy = user);
+
+    //        Assert.IsTrue(result);
+    //    }
+
+    //    [TestMethod]
+    //    public void CanbeCancelledBy_AnotherUserCancelling_ReturnsFalse()
+    //    {
+    //        var reservation = new Reservation { MadeBy = new User() };
+
+    //        var result = reservation.CanBeCancelledBy(new User());
+
+    //        Assert.IsFalse(result);
+    //    }
+    //}
+    #endregion
+
+    //<---- Using NUnit Test ----->
+    #region
+    [TestFixture]
     public class ReservationTests
     {
-        [TestMethod]
+        [Test]
         //Good Naming Convention in naming the method you have to test: NameOfTheMethod_Scenario_ExpectedBehaviour()
         public void CanBeCancelledBy_AdminCancelling_ReturnsTrue()
         {
@@ -17,13 +64,13 @@ namespace TestNinja.UnitTests
             var reservation = new Reservation();
 
             //Act - Here we specify the method or function that we want to test and supply the parameters/arguments needed
-            var result = reservation.CanBeCancelledBy( new User() {IsAdmin = true });
+            var result = reservation.CanBeCancelledBy(new User() { IsAdmin = true });
 
             //Assert - Here we verify that result if correct.
-            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
         }
 
-        [TestMethod]
+        [Test]
         public void CanBeCancelledBy_SameUsercCanceling_ReturnsTrue()
         {
             var user = new User();
@@ -31,17 +78,18 @@ namespace TestNinja.UnitTests
 
             var result = reservation.CanBeCancelledBy(reservation.MadeBy = user);
 
-            Assert.IsTrue(result);
+            Assert.That(result, Is.True);
         }
 
-        [TestMethod]
+        [Test]
         public void CanbeCancelledBy_AnotherUserCancelling_ReturnsFalse()
         {
             var reservation = new Reservation { MadeBy = new User() };
 
             var result = reservation.CanBeCancelledBy(new User());
 
-            Assert.IsFalse(result);
+            Assert.That(result, Is.False);
         }
     }
+    #endregion
 }
