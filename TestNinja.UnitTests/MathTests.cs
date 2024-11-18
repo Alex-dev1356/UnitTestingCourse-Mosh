7bb9dd1483f46a27928f4b6446155fa6ffc8c842
Alex-dev1356 <asoliven26@yahoo.com>
2024-11-19 05:06:10 +0800

    Ignoring a Test

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -133,6 +133,40 @@ namespace TestNinja.UnitTests
 
     //<---- Parametarized Tests ----->
     #region
+    //[TestFixture]
+    //public class MathTests
+    //{
+    //    private Fundamentals.Math _math;
+
+    //    //SetUp Attribute
+    //    [SetUp]
+    //    public void SetUp()
+    //    {
+    //        _math = new Fundamentals.Math();
+    //    }
+
+    //    [Test]
+    //    public void Add_WhenCalled_ReturnsSumOfArguments()
+    //    {
+    //        var result = _math.Add(1, 2);
+
+    //        Assert.That(result, Is.EqualTo(3));
+    //    }
+
+    //    [Test]
+    //    [TestCase(2, 1, 2)]
+    //    [TestCase(1, 2, 2)]
+    //    [TestCase(1, 1, 1)]
+    //    public void Max_WhenCalled_ReturnsTheGreaterArgument(int a, int b, int expectedResult)
+    //    {
+    //        var result = _math.Max(a, b);
+
+    //        Assert.That(result, Is.EqualTo(expectedResult));
+    //    }
+    #endregion
+
+    //<---- Ignoring Test ----->
+    #region
     [TestFixture]
     public class MathTests
     {
@@ -145,7 +179,9 @@ namespace TestNinja.UnitTests
             _math = new Fundamentals.Math();
         }
 
+        //Using the Ignore Attribute
         [Test]
+        [Ignore("Sample of Ignoring this test cause I wanted to.")]
         public void Add_WhenCalled_ReturnsSumOfArguments()
         {
             var result = _math.Add(1, 2);
