4ab6038814664c38f368ee1f7bc95a6d2f217337
Alex-dev1356 <asoliven26@yahoo.com>
2024-11-18 12:46:44 +0800

    Black-box Testing

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -21,5 +21,45 @@ namespace TestNinja.UnitTests
             Assert.That(result, Is.EqualTo(3));
         }
 
+        //Black-box Testing
+        //On testing the Max Method we have morethan one execution path, the first one is when the first argument is greater, second is 
+        //when the second argument is greater and lastly if BOTH the Arguments are equal.
+
+        //When First Argument is greater
+        [Test]
+        public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
+        {
+            var math = new Fundamentals.Math();
+
+            var result = math.Max(2,1);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+
+        //When Second Argument is greater
+        [Test]
+        public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
+        {
+            var math = new Fundamentals.Math();
+
+            var result = math.Max(1, 2);
+
+            Assert.That(result, Is.EqualTo(2));
+        }
+
+        //When Both Arguments are equal
+        [Test]
+        public void Max_ArgumentsAreEqual_ReturnsTheArgument()
+        {
+            var math = new Fundamentals.Math();
+
+            var result = math.Max(1, 1);
+
+            Assert.That(result, Is.EqualTo(1));
+        }
+
+        //Key Takeaway: When Testing a Method you have to think about all the possible Execution Paths.The best way to write test for a method is to
+        //THINK OF IT AS A BLACK BOX. This means you have to think about all the possible Execution Paths.
     }
 }
