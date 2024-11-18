39fd0a2d54691de7ad3b0ade308e3f192ad1ac92
Alex-dev1356 <asoliven26@yahoo.com>
2024-11-18 13:03:53 +0800

    Set Up Attribute for writing a cleaner way of Tests

--- TestNinja.UnitTests/MathTests.cs
+++ TestNinja.UnitTests/MathTests.cs
@@ -8,15 +8,87 @@ using TestNinja.Fundamentals;
 
 namespace TestNinja.UnitTests
 {
+    //<---- Testing the Math Class ---->
+    #region
+    //[TestFixture]
+    //public class MathTests
+    //{
+    //    [Test]
+    //    public void Add_WhenCalled_ReturnsSumOfArguments()
+    //    {
+    //        var math = new Fundamentals.Math();
+
+    //        var result = math.Add(1, 2);
+
+    //        Assert.That(result, Is.EqualTo(3));
+    //    }
+
+    //    //Black-box Testing
+    //    //On testing the Max Method we have morethan one execution path, the first one is when the first argument is greater, second is 
+    //    //when the second argument is greater and lastly if BOTH the Arguments are equal.
+
+    //    //When First Argument is greater
+    //    [Test]
+    //    public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
+    //    {
+    //        var math = new Fundamentals.Math();
+
+    //        var result = math.Max(2,1);
+
+    //        Assert.That(result, Is.EqualTo(2));
+    //    }
+
+
+    //    //When Second Argument is greater
+    //    [Test]
+    //    public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
+    //    {
+    //        var math = new Fundamentals.Math();
+
+    //        var result = math.Max(1, 2);
+
+    //        Assert.That(result, Is.EqualTo(2));
+    //    }
+
+    //    //When Both Arguments are equal
+    //    [Test]
+    //    public void Max_ArgumentsAreEqual_ReturnsTheArgument()
+    //    {
+    //        var math = new Fundamentals.Math();
+
+    //        var result = math.Max(1, 1);
+
+    //        Assert.That(result, Is.EqualTo(1));
+    //    }
+
+    //    //Key Takeaway: When Testing a Method you have to think about all the possible Execution Paths.The best way to write test for a method is to
+    //    //THINK OF IT AS A BLACK BOX. This means you have to think about all the possible Execution Paths.
+    //}
+    #endregion
+
+    //<--- SetUp and TearDown ---->
+    #region
     [TestFixture]
     public class MathTests
     {
+        //Writing a cleaner way in writing tests by using SetUp and TearDown. In NUnit we two special attributes SetUp and TearDown. 
+        //We can create a method here and decorate it with a SetUp attribute and then Nunit Test runner will call that method before
+        //running each test and it is a great opportunity for us to initialize our Math Objects. Simillarly we can use TearDown attribute
+        //and then NUnit Test runner will call that method after running each test. Teardown Attribute is often used in Integration Tests.
+
+        private Fundamentals.Math _math;
+
+        //SetUp Attribute
+        [SetUp]
+        public void SetUp()
+        {
+            _math = new Fundamentals.Math();
+        }
+
         [Test]
         public void Add_WhenCalled_ReturnsSumOfArguments()
         {
-            var math = new Fundamentals.Math();
-
-            var result = math.Add(1, 2);
+            var result = _math.Add(1, 2);
 
             Assert.That(result, Is.EqualTo(3));
         }
@@ -29,21 +101,16 @@ namespace TestNinja.UnitTests
         [Test]
         public void Max_FirstArgumentIsGreater_ReturnsTheFirstArgument()
         {
-            var math = new Fundamentals.Math();
-
-            var result = math.Max(2,1);
+            var result = _math.Max(2, 1);
 
             Assert.That(result, Is.EqualTo(2));
         }
-
 
         //When Second Argument is greater
         [Test]
         public void Max_SecondArgumentIsGreater_ReturnsTheSecondArgument()
         {
-            var math = new Fundamentals.Math();
-
-            var result = math.Max(1, 2);
+            var result = _math.Max(1, 2);
 
             Assert.That(result, Is.EqualTo(2));
         }
@@ -62,4 +129,6 @@ namespace TestNinja.UnitTests
         //Key Takeaway: When Testing a Method you have to think about all the possible Execution Paths.The best way to write test for a method is to
         //THINK OF IT AS A BLACK BOX. This means you have to think about all the possible Execution Paths.
     }
+
+    #endregion
 }
