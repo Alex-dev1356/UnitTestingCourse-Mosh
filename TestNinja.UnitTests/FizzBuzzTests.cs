bb1f3df676b47b0051e8dfe8d3e2442ecc2b6203
Alex-dev1356 <asoliven26@yahoo.com>
2024-12-01 10:43:53 +0800

    FizzBuzz Exercise

---
+++ TestNinja.UnitTests/FizzBuzzTests.cs
@@ -0,0 +1,49 @@
+﻿using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TestNinja.Fundamentals;
+
+namespace TestNinja.UnitTests
+{
+    //FizzBuzz Exercise
+    [TestFixture]
+    public class FizzBuzzTests
+    {
+        [Test]
+        public static void GetOutput_DivisibleBy3And5_ReturnsFizzBuzz()
+        {
+            var result = FizzBuzz.GetOutput(15);
+
+            Assert.That(result, Is.EqualTo("FizzBuzz"));
+        }
+
+        [Test]
+        public void GetOutput_DivisibleBy3_ReturnsFizz()
+        {
+            var result = FizzBuzz.GetOutput(6);
+
+            Assert.That(result, Is.EqualTo("Fizz"));
+        }
+
+        [Test]
+        public void GetOutput_DivisibleBy5_ReturnsBuzz()
+        {
+            var result = FizzBuzz.GetOutput(10);
+
+            Assert.That(result, Is.EqualTo("Buzz"));
+        }
+
+        [Test]
+        [TestCase(7)]
+        [TestCase(-1)]
+        public void GetOutput_NotDivisibleBy3or5AndNegativeNumber_ReturnsSameNumber(int inputNumber)
+        {
+            var result = FizzBuzz.GetOutput(inputNumber);
+
+            Assert.That(result, Is.EqualTo(inputNumber.ToString()));
+        }
+    }
+}
